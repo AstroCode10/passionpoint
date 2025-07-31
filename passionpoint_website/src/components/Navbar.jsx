2f60d0f992b6d20babc67b31f6fad2ef8c868689
Thisum Samarasinghe <sathisumds@gmail.com>
2025-07-31 09:30:44 +0530

    Update Navbar.jsx

--- passionpoint_website/src/components/Navbar.jsx
+++ passionpoint_website/src/components/Navbar.jsx
@@ -1,1 +1,18 @@
 // components/Navbar.jsx - Placeholder content
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 p-4 text-white flex justify-between items-center">
+      <h1 className="text-xl font-bold">PassionPoint</h1>
+      <div className="space-x-4">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
