9240e7b68f96260c9a66eef874749af13fa66b8a
Thisum Samarasinghe <sathisumds@gmail.com>
2025-08-01 17:39:33 +0530

    Update App.js

--- passionpoint_website/src/App.js
+++ passionpoint_website/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import Home from './Home';
+import React from "react";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold mb-4 text-blue-600">Welcome to PassionPoint</h1>
       <Home />
     </div>
   );
