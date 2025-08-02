4d59b134bf9e3a2bab9168dc806f49e00dd61a9c
AstroCode10 <sathisumds@gmail.com>
2025-08-02 11:41:22 +0530

    Updated tailwind.config

--- passionpoint_website/tailwind.config.js
+++ passionpoint_website/tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",  // This includes all your components
+    "./src/styles/globals.css"     // Include this too just to be sure
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
