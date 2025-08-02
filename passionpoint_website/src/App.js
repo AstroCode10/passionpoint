7f3a7dfcb6359d52db40167a04995a735e03e994
Thisum Samarasinghe <sathisumds@gmail.com>
2025-08-02 06:33:06 +0530

    Update App.js

--- passionpoint_website/src/App.js
+++ passionpoint_website/src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="text-center p-10">
-      <h1 className="text-4xl font-bold mb-4 text-blue-600">Welcome to PassionPoint</h1>
       <Home />
     </div>
   );
